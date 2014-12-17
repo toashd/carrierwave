6814473107d1480e492813640c5c55908bccc2d9
Tobias Schmid <tobias.schmid@stampay.com>
2014-12-17 11:45:11 +0100

    Recognize uri reserved characters in path encoding

--- lib/carrierwave/utilities/uri.rb
+++ lib/carrierwave/utilities/uri.rb
@@ -6,8 +6,8 @@ module CarrierWave
 
     private
       def encode_path(path)
-        # based on Ruby < 2.0's URI.encode
-        safe_string = URI::REGEXP::PATTERN::UNRESERVED + '\/'
+        # based on Ruby's URI.encode (see lib/uri/common.rb)
+        safe_string = URI::REGEXP::PATTERN::UNRESERVED + URI::REGEXP::PATTERN::RESERVED + '\/'
         unsafe = Regexp.new("[^#{safe_string}]", false)
 
         path.to_s.gsub(unsafe) do
